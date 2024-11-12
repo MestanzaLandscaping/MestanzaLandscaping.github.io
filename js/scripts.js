73e1bce96ddb3a14857f181c017c99dbb038b1a1
MestanzaLandscaping <162927706+MestanzaLandscaping@users.noreply.github.com>
2024-11-11 21:36:04 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,29 @@ const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg");
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-        msg.innerHTML = "Message sent successfully"
-        setTimeout(function(){
-            msg.innerHTML = ""
-        }, 5000)
-        form.reset()
-    })
-    .catch(error => console.error('Error!', error.message))
+  e.preventDefault();
+  
+  msg.innerHTML = "Sending...";
+
+  fetch(scriptURL, { 
+    method: 'POST', 
+    body: new FormData(form)
+  })
+  .then(response => response.json()) 
+  .then(data => {
+    msg.innerHTML = "Message sent successfully";
+    setTimeout(function() {
+        msg.innerHTML = "";
+    }, 5000);
+    form.reset();
+  })
+  .catch(error => {
+    console.error('Error!', error.message);
+    msg.innerHTML = "Failed to send the message. Please try again.";
+    setTimeout(function() {
+        msg.innerHTML = "";
+    }, 5000);
+  });
 });
 
 
